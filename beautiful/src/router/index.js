7fc0cf6c71d50ee302e9e6b8febd76c982c14dc6
kobe <lichune@sina.com>
2017-09-18 18:01:37 +0800

    完善了路由

--- beautiful/src/router/index.js
+++ beautiful/src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Home from '@/components/Home/Home'
+import Cart from '@/components/Cart/Cart'
+import Detail from '@/components/Detail/Detail'
+import List from '@/components/List/List'
+import Mine from '@/components/Mine/Mine'
+import Activity from '@/components/Activity/Activity'
 
 Vue.use(Router)
 
@@ -8,8 +13,34 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/cart',
+      name: 'Cart',
+      component: Cart
+    },
+    {
+      path: '/detail',
+      name: 'Detail',
+      component: Detail
+    },
+    {
+      path: '/list',
+      name: 'List',
+      component: List
+    },
+    {
+      path: '/mine',
+      name: 'Mine',
+      component: Mine
+    },
+    {
+      path: '/activity',
+      name: 'Activity',
+      component: Activity
     }
+    
   ]
 })
